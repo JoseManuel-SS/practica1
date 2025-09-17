bb2a3e21e080f84517aedecd5878fbfcee70cc06
JoseManuel-SS <josemsantelizs@gmail.com>
2025-09-16 23:40:26 -0400

    Segundo commit: Crear el archivo main.js

---
+++ main.js
@@ -0,0 +1,9 @@
+const name="José Santeliz"
+const edad=20
+const estado="Soltero"
+const carrera="Informática"
+
+console.log("Hola, mi nombre es "+name)
+console.log("Tengo "+edad)
+console.log("Soy "+estado)
+console.log("Estudio la carrera de "+carrera)
